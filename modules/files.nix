de7e1aa0fc20aa96707fc5428443622053003639
for { kys() } <yyeet62425@gmail.com>
2025-01-30 14:18:12 +0530

    :nix:tmux|starship: move to hm

--- modules/files.nix
+++ modules/files.nix
@@ -1,19 +1,9 @@
-{
-  config,
-  pkgs,
-  ...
-}: {
+{pkgs, ...}: {
   home = {
     file = {
       ".icons/WhiteSur-cursors" = {
         source = "${pkgs.whitesur-cursors}/share/icons/WhiteSur-cursors";
         recursive = true;
-      };
-
-      "${config.home.homeDirectory}/.config/starship.toml" = {
-        source =
-          config.lib.file.mkOutOfStoreSymlink
-          "${config.home.homeDirectory}/.config/nixcfg/starship/starship.toml";
       };
     };
   };
