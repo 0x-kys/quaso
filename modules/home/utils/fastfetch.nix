43c804f989240251e424391eaada8ba48bbb416f
for { kys() } <yyeet62425@gmail.com>
2025-02-17 21:55:24 +0530

    :fastfetch: save config to store for pure build

--- modules/home/utils/fastfetch.nix
+++ modules/home/utils/fastfetch.nix
@@ -2,4 +2,9 @@
   programs.fastfetch = {
     enable = true;
   };
+
+  home.file.".config/fastfetch/config.jsonc".source = builtins.path {
+    path = ../../../extras/fastfetch/config.jsonc;
+    name = "fastfetch-config";
+  };
 }
